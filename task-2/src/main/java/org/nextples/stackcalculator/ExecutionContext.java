c8da101f6daa3c174c1c372eb08e9582600636f4
nextples <gankeev@gmail.com>
2024-02-25 22:50:46 +0700

    add getStack and getParameters

--- task-2/src/main/java/org/nextples/stackcalculator/ExecutionContext.java
+++ task-2/src/main/java/org/nextples/stackcalculator/ExecutionContext.java
@@ -5,39 +5,49 @@ import java.util.HashMap;
 import java.util.Stack;
 
 public class ExecutionContext {
-    private static ExecutionContext instance = null;
-    private static Stack<Double> stack = null;
-    private static HashMap<String, Double> map = null;
+    private final Stack<Double> stack;
+    private final HashMap<String, Double> parameters;
 
-    private ExecutionContext() {}
-
-    public static ExecutionContext init() {
-        if(instance == null) {
-            instance = new ExecutionContext();
-            stack = new Stack<>();
-            map = new HashMap<>();
-        }
-        return instance;
+    private ExecutionContext() {
+        this.stack = new Stack<>();
+        this.parameters = new HashMap<>();
     }
 
-    public double stackPeek() throws EmptyStackException {
-        return stack.peek();
+//    public static ExecutionContext init() {
+//        if(instance == null) {
+//            instance = new ExecutionContext();
+//            stack = new Stack<>();
+//            map = new HashMap<>();
+//        }
+//        return instance;
+//    }
+
+    public Stack getStack() {
+        return stack;
     }
 
-    public void stackPop() throws EmptyStackException {
-        stack.pop();
+    public HashMap getParameters() {
+        return parameters;
     }
 
-    public void stackPush(double val) {
-        stack.push(val);
-    }
-
-    public double mapGet(String key) {
-        return map.get(key);
-    }
-
-    public void mapPut(String key, double val) {
-        map.put(key, val);
-    }
+//    public double stackPeek() throws EmptyStackException {
+//        return stack.peek();
+//    }
+//
+//    public void stackPop() throws EmptyStackException {
+//        stack.pop();
+//    }
+//
+//    public void stackPush(double val) {
+//        stack.push(val);
+//    }
+//
+//    public double mapGet(String key) {
+//        return parameters.get(key);
+//    }
+//
+//    public void mapPut(String key, double val) {
+//        parameters.put(key, val);
+//    }
 
 }
